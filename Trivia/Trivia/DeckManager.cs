0cf52b188589557d75d9d72645c8048cbb729118
Vianney Chepeau <vianney.chepeau@cdbdx.biz>
2022-08-30 17:05:32 +0200

    Implementation of AddQuestion in DeckManager

--- Trivia/Trivia/DeckManager.cs
+++ Trivia/Trivia/DeckManager.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Trivia
 {
     public class DeckManager
     {
+        private readonly Dictionary<Categories, Deck> _decks = new Dictionary<Categories, Deck>();
+
         public void AddQuestion(Question question, Categories pop)
         {
-            throw new NotImplementedException();
+            _decks.GetValueOrDefault(pop).AddQuestion(question);
         }
 
         public Question DrawQuestion(Categories pop)
